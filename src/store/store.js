a214a0fe34e93c8da63742a6d97980cab973ed01
julimunioz <103261440+julimunioz@users.noreply.github.com>
2023-05-19 10:07:38 -0300

    primer commit

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { uiSlice, calendarSlice } from "./";
+
+export const store = configureStore({
+    reducer: {
+        ui: uiSlice.reducer,
+        calendar: calendarSlice.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
+});
