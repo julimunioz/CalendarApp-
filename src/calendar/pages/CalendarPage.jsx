1843b9b053c7a9ee0763c21acdbd447c419e39b8
julimunioz <103261440+julimunioz@users.noreply.github.com>
2023-05-24 15:37:58 -0300

    agregar funcionalidades

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,20 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { Navbar, CalendarEvent, CalendarModal, FabAddNew, FabDelete } from "../"
 
 import { localizer, getMessagesES } from '../../helpers';
-import { useUiStore, useCalendarStore } from '../../hooks';
+import { useUiStore, useCalendarStore, useAuthStore } from '../../hooks';
 
 export const CalendarPage = () => {
 
+  const { user } = useAuthStore();
   const { openDateModal } = useUiStore();
-  const { events, setActiveEvent } = useCalendarStore();
+  const { events, setActiveEvent, startLoadingEvent } = useCalendarStore();
 
   const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
 
   const eventStyleGetter = (event, start, end, isSelected) => {
+    const isMyEvent = ( user.uid === event.user._id) || ( user.uid === event.user._uid)
+    
+    const style = {
+      backgroundColor: isMyEvent ? '#347cf7' : '#465660',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {style};
+  
   }
 
   const onDoubleClick = ( event ) => {
@@ -30,6 +42,11 @@ export const CalendarPage = () => {
     setLastView(event);
   }
 
+  useEffect(() => {
+    startLoadingEvent();  
+  }, [])
+  
+
   return (
     <>
       <Navbar/>
@@ -41,7 +58,7 @@ export const CalendarPage = () => {
         defaultView={lastView}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 'calc( 100vh - 80px)' }}
+        style={{ height: 'calc( 100vh - 80px)', eventStyleGetter }}
         messages={getMessagesES()}
         eventPropGetter={eventStyleGetter}
         components={{
--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,10 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
-import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent, onLoadEvents } from "../store";
+import { calendarApi } from "../api";
+import { convertEventsToDateEvents } from "../helpers";
+import Swal from "sweetalert2";
 
 export const useCalendarStore = () => {
 
     const dispatch = useDispatch();
     const { events, activeEvent } = useSelector(state => state.calendar);
+    const { user } = useSelector(state => state.auth);
     
     const setActiveEvent = ( calendarEvent ) => {
         dispatch(onSetActiveEvent(calendarEvent))
@@ -12,18 +16,58 @@ export const useCalendarStore = () => {
 
     const startSavingEvent = async ( calendarEvent ) => {
 
-            if (calendarEvent._id) {
+        try {
+    
+            if (calendarEvent.id) {
+              
                 //actualizando
-                dispatch( onUpdateEvent({...calendarEvent}) );
+                await calendarApi.put(`/events/${calendarEvent.id}`, calendarEvent)
+                dispatch( onUpdateEvent({...calendarEvent, user}) );
+                return;
+        
             } else {
+              
                 //creando
-                dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }) );
+                const {data} = await calendarApi.post('/events', calendarEvent);
+                dispatch( onAddNewEvent({ ...calendarEvent, id: data.evento.id, user }) );
             }
+        
+        } catch (error) {
+            console.log(error);
+            Swal.fire('Error al guardar', error.response.data.msg, 'error');
+        }
     }
 
-    const startDeletingEvent = () => {
-        dispatch( onDeleteEvent() );
+    const startDeletingEvent = async () => {
+
+        try {
+
+            await calendarApi.delete(`/events/${activeEvent.id}`)
+            dispatch( onDeleteEvent() );
+         
+        } catch (error) {
+            console.log(error);
+            Swal.fire('Error al eliminar', error.response.data.msg, 'error');
+        }
+
+        
     }
+
+    const startLoadingEvent = async () => {
+        try {
+
+            const {data} = await calendarApi.get('/events');
+            const events = convertEventsToDateEvents(data.eventos);
+            dispatch(onLoadEvents (events));
+        
+        } catch (error) {
+        
+            console.log('Error cargando eventos');
+            console.log(error);
+        
+        }
+    }
+ 
 
     return {
 
@@ -36,5 +80,6 @@ export const useCalendarStore = () => {
         startDeletingEvent,
         setActiveEvent,
         startSavingEvent,
+        startLoadingEvent
     }
 }
